dd9135f9af325c3fa3e71db700007db2145c2d9b
Brandon <blongen1@ycp.edu>
2016-03-12 15:29:06 -0500

    Testing commit (just added spacing)

--- Ch@/junit/junit/model/GuessingGameTest.java
+++ Ch@/junit/junit/model/GuessingGameTest.java
@@ -13,16 +13,19 @@ public class GuessingGameTest {
 	@Before
 	public void setUp() {
 		model = new GuessingGame();
+		
 	}
 	
 	@Test
 	public void testSetMin() {
 		model.setMin(1);
 		assertEquals(1, model.getMin());
+		
 	}
 	
 	public void testSetMax() {
 		model.setMax(100);
 		assertEquals(100, model.getMax());
+		
 	}
 }
