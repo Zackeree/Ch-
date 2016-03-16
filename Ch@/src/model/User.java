926f67d2c774195846250cf87d718ef97bb66a30
Zackeree <zhenry@ycp.edu>
2016-03-16 13:00:42 -0400

    User is implemented*
    *these statements may not accurately depict the nature of User.

--- Ch@/src/model/User.java
+++ Ch@/src/model/User.java
@@ -1,14 +1,30 @@
 package model;
 
+import model.Room;
+import model.Profile;
+
 public class User {
 	private String id, password, email;
 	private int infraction;
 	private boolean admin;
-
+	private Room room;
+	
+	public User(String id, String password, String email) {
+		this.id = id;
+		this.password = password;
+		this.email = email;
+		this.admin = false;
+	}
+	
+	/******These methods are WIP*********************
 	public void log_in(String id, String password) {
 		if(this.id == id && this.password == password)
 	}
 	
+	public void log_out(User model)	{
+		
+	}
+	*************************************************/
 	public String getID() {
 		return this.id;
 	}
@@ -21,12 +37,26 @@ public class User {
 		return this.password;
 	}
 	
+	public void setPassword(String password)
+	{
+		this.password = password;
+	}
+	
 	public String getEmail() {
 		return this.email;
 	}
 	
+	public void setEmail() {
+		this.email = email;
+	}
+	
 	public int getInfraction() {
 		return this.infraction;
+	}
+	
+	public void setInfraction(int time) {
+		if(this.admin == true)
+			this.infraction = time;
 	}
 	
 	public boolean isAdmin() {
@@ -35,4 +65,33 @@ public class User {
 		else
 			return false;
 	}
+	
+	// This method is a toggle method. If already an admin, calling it removes admin status.
+	public void setAdmin() {
+		if(!this.admin)
+			this.admin = true;
+		else if(this.admin)
+			this.admin = false;
+	}
+	
+	// Not properly implemented
+	public Room createRoom() {
+		return this.room;
+	}
+	
+	/* following methods are NOT properly implemented, and require admin privileges** 
+	public void banUser(User model, int time) {
+		if(this.admin == true)
+			model.setInfraction(time);
+	}
+	
+	public void kickUser(User model) {
+		if(this.admin == true)
+			model.log_out();
+	}
+	
+	public void ORDel() {
+		
+	}
+	*********************************************************************************/
 }
