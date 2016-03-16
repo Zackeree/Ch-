9c0d69644bcf488e6b191bff6308673166f9ae8a
Zackeree <zhenry@ycp.edu>
2016-03-16 12:26:57 -0400

    Added a test page, started filling out User.

--- Ch@/src/model/User.java
+++ Ch@/src/model/User.java
@@ -1,5 +1,34 @@
 package model;
 
 public class User {
+	private String id, password, email;
+	private int infraction;
+	private boolean admin;
 
+	public void log_in(String id, String password) {
+		if(this.id == id && this.password == password)
+	}
+	
+	public String getID() {
+		return this.id;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getEmail() {
+		return this.email;
+	}
+	
+	public int getInfraction() {
+		return this.infraction;
+	}
+	
+	public boolean isAdmin() {
+		if(this.admin == true)
+			return true;
+		else
+			return false;
+	}
 }
