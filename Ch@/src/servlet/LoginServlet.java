f3bd52f0bfe2bc3701a3261114c6d3f04145e8f3
Zackeree <zhenry@ycp.edu>
2016-03-18 13:44:52 -0400

    LoginServlet finished?

--- Ch@/src/servlet/LoginServlet.java
+++ Ch@/src/servlet/LoginServlet.java
@@ -1,10 +1,38 @@
 package servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet {
+import model.FakeDatabase;
 
+public class LoginServlet extends HttpServlet {
+	
+	FakeDatabase db = new FakeDatabase();
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String id = request.getParameter("id");
+        String pass = request.getParameter("pass");
+        
+        if(FakeDatabase.validate(db, id, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("Welcome");
+            rs.forward(request, response);
+        }
+        else
+        {
+           out.println("Username or Password incorrect");
+           RequestDispatcher rs = request.getRequestDispatcher("index.html");
+           rs.include(request, response);
+        }
+    } 
 }
