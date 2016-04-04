2e40f3a360f7e6e8e26d279e63efb10615b6b8dd
Zackeree <zhenry@ycp.edu>
2016-04-04 12:22:18 -0400

    Dummied out RoomTest, filling in now.

--- Ch@/junit/junit/model/RoomTest.java
+++ Ch@/junit/junit/model/RoomTest.java
@@ -10,12 +10,88 @@ import model.User;
 
 public class RoomTest {
 	private Room model, model2;
+	private User uModel, uModel2;
 
 	@Before
 	public void setUp() {
 		model = new Room("test", false, null);
 		model2 = new Room("RealZ", true, new User("id", "pass", "email"));
+		uModel = new User(null, null, null);
+		uModel2 = new User("id", "pass", "email");
 	}
 	
+	@Test
+	public void testPurge() {
+		
+	}
 	
+	@Test
+	public void testDelete() {
+		
+	}
+	
+	@Test
+	public void testJoin() {
+		
+	}
+	
+	@Test
+	public void testInvite() {
+		
+	}
+	
+	@Test
+	public void testKick() {
+		
+	}
+	
+	@Test
+	public void testGetName() {
+		
+	}
+	
+	@Test
+	public void testSetName() {
+		
+	}
+
+	@Test
+	public void testGetUsers() {
+		
+	}
+
+	@Test
+	public void testSetUsers() {
+		
+	}
+
+	@Test
+	public void testGetMessages() {
+		
+	}
+
+	@Test
+	public void testSetMessages() {
+		
+	}
+
+	@Test
+	public void testIsPrivate() {
+		
+	}
+
+	@Test
+	public void testSetPrivate() {
+		
+	}
+
+	@Test
+	public void testGetOwner() {
+		
+	}
+
+	@Test
+	public void testSetOwner() {
+		
+	}
 }
