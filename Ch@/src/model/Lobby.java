5dbad748caefb3a2d9c1bd2995624067535fa33b
Zackeree <zhenry@ycp.edu>
2016-04-04 13:24:56 -0400

    filled in Lobby

--- Ch@/src/model/Lobby.java
+++ Ch@/src/model/Lobby.java
@@ -1,5 +1,24 @@
 package model;
 
-public class Lobby {
+import java.util.ArrayList;
 
+public class Lobby {
+	private ArrayList<Room> rooms;
+	private ArrayList<User> users;
+	
+	public ArrayList<Room> getRooms() {
+		return this.rooms;
+	}
+	
+	public void setRooms(ArrayList<Room> rooms) {
+		this.rooms = rooms;
+	}
+	
+	public ArrayList<User> getUsers() {
+		return this.users;
+	}
+	
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
+	}
 }
