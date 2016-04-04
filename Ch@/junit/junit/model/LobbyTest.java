4f499581d23a0bdf655865f0bbce6dd58320417c
Zackeree <zhenry@ycp.edu>
2016-04-04 13:34:37 -0400

    filled out lobbyTest partially

--- Ch@/junit/junit/model/LobbyTest.java
+++ Ch@/junit/junit/model/LobbyTest.java
@@ -1,5 +1,36 @@
 package junit.model;
 
-public class LobbyTest {
+import model.Lobby;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+
+public class LobbyTest {
+	private Lobby model;
+	
+	@Before
+	public void setUp() {
+		model = new Lobby();
+	}
+	
+	@Test
+	public void testGetRooms() {
+		
+	}
+
+	@Test
+	public void testSetRooms() {
+		
+	}
+	
+	@Test
+	public void testGetUsers() {
+		
+	}
+	
+	@Test
+	public void testSetUsers() {
+		
+	}
 }
