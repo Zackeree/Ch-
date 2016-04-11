ff613b4102cb9498bfd7b220d9a88c686eb52bfc
Zackeree <zhenry@ycp.edu>
2016-04-11 13:30:18 -0400

    Moved lab06

---
+++ Ch@/src/persist/IDatabase.java
@@ -0,0 +1,16 @@
+package persist;
+
+import java.util.List;
+
+import model.Author;
+import model.Book;
+import model.Pair;
+
+public interface IDatabase {
+	public List<Pair<Author, Book>> findAuthorAndBookByTitle(String title);
+	public List<Pair<Author, Book>> findAuthorAndBookByAuthorLastName(String lastName);
+	public Integer insertBookIntoBooksTable(String title, String isbn, String lastName, String firstName);
+	public List<Pair<Author, Book>> findAllBooksWithAuthors();
+	public List<Author> findAllAuthors();
+	public List<Author> removeBookByTitle(String title);		
+}
