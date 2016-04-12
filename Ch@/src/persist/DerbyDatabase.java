d2e5b32ba9d4a7aea7f4e1f7d7813771e8e0834c
Zackeree <Zackeree@10.0.0.93>
2016-04-12 13:55:55 -0400

    Pointed DB and SQL at C drive instead of H drive

--- Ch@/src/persist/DerbyDatabase.java
+++ Ch@/src/persist/DerbyDatabase.java
@@ -587,7 +587,7 @@ public class DerbyDatabase implements IDatabase {
 	// TODO: You will need to change this location to the same path as your workspace for this example
 	// TODO: Change it here and in SQLDemo under CS320_Lab06->edu.ycp.cs320.sqldemo	
 	private Connection connect() throws SQLException {
-		Connection conn = DriverManager.getConnection("jdbc:derby:H:/CS320/library.db;create=true");		
+		Connection conn = DriverManager.getConnection("jdbc:derby:C:/CS320/library.db;create=true");		
 		
 		// Set autocommit to false to allow multiple the execution of
 		// multiple queries/statements as part of the same transaction.
