66bf98b8f099e30774e5d393e78b6a9359c04eea
Zackeree <Zackeree@10.0.0.93>
2016-04-14 20:59:14 -0400

    Instantiate DB in LobbyController

--- Ch@/src/controller/LobbyController.java
+++ Ch@/src/controller/LobbyController.java
@@ -1,5 +1,22 @@
 package controller;
 
-public class LobbyController {
+import java.util.ArrayList;
 
+import model.*;
+import persist.DatabaseProvider;
+import persist.DerbyDatabase;
+import persist.IDatabase;
+
+public class LobbyController {
+	
+	private IDatabase db = null;
+
+	public LobbyController() {
+		DatabaseProvider.setInstance(new DerbyDatabase());
+		db = DatabaseProvider.getInstance();
+	}
+	/*
+	public ArrayList<User> getUsersInLobby() {
+		
+	}*/
 }
