a4216ecc449a90e6350ea9b9b910d10396c5eaf0
Brandon <blongen1@ycp.edu>
2016-04-23 16:41:58 -0400

    Automatically updates to bottom, unless user scrolls certain dist. away

--- Ch@/war/_view/js/script.js
+++ Ch@/war/_view/js/script.js
@@ -7,9 +7,24 @@ $(function() {
     });
 });
 
+// Height = 480px;
+
+function updateScroll(){
+	var scroll = document.getElementById("chatMessages");
+	//scrollBottom will equal scrollTop if scrolled to bottom
+	var scrollBottom = (scroll.scrollTop + scroll.scrollHeight - 480)/2;
+	// If user is close to bottom, automatically update to bottom message
+	// This accounts for a new messages not making scrollBottom == scrollTop perfectly
+	if (scrollBottom - 30 <= scroll.scrollTop) {
+		scroll.scrollTop = scroll.scrollHeight;
+	}
+}
+
 
 function auto_load(){
     $("#chatMessages").load(location.href + " #chatMessages");
-};
+    var scroll = document.getElementById("chatMessages");
+    updateScroll();
+}
 
-setInterval(auto_load, 1000);
+setInterval(auto_load, 500);
