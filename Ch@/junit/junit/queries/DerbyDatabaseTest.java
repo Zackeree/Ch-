f129296dc3e402745f6a715d4f8f2abf82cdae4f
Zackeree <zhenry@ycp.edu>
2016-05-02 12:25:29 -0400

    2 queries down, a million to go

--- Ch@/junit/junit/queries/DerbyDatabaseTest.java
+++ Ch@/junit/junit/queries/DerbyDatabaseTest.java
@@ -2,16 +2,50 @@ package junit.queries;
 
 import persist.DerbyDatabase;
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import model.Message;
+import model.User;
+
 public class DerbyDatabaseTest {
 
-	DerbyDatabase db;
+	private DerbyDatabase db;
+	private User user;
+	private Message message;
 	
-	public DerbyDatabaseTest() {
+	@Before
+	public void setUp() {
 		db = new DerbyDatabase();
+		user = new User(null, null, null);
+		message = new Message(null);
 	}
 	
+	@Test
+	public void testRetrieveUser() {
+		user = db.retrieveUser("zhenry");
+		assertEquals("zhenry", user.getID());
+		user = db.retrieveUser("test");
+		assertEquals("test", user.getID());
+		user = db.retrieveUser("blonge");
+		assertNotEquals("false", user.getID());
+	}
+	
+	@Test
+	public void testRetrieveMessages() {
+		ArrayList <Message> messages = new ArrayList<Message>();
+		messages = db.retrieveMessages();
+		message = messages.get(0);
+		assertTrue(message.getText().contains("this is a string"));
+		message = messages.get(1);
+		assertTrue(message.getText().contains("this is another string"));
+		message = messages.get(2);
+		assertTrue(message.getText().contains("this is sparta"));
+		message = messages.get(3);
+		assertTrue(message.getText().contains("No this is Patrick."));
+	}
 	
 }
